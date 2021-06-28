90c56d11b76e819d79ed093bc18d40a4bf77f74c
Sunggat Aiymbay <sunggat.aiymbay@icloud.com>
2021-06-28 22:05:29 +0600

    Fix

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from fastapi import FastAPI
 from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
-from predictor import *
+from predictor import read_imagefile, predict
 from typing import Optional
 
 
 app_desc = """<h2>Try this app by uploading any image</h2>
 <h2>API for Face Spoofing</h2>"""
-
-class Item(BaseModel):
-    file: UploadFile = File(...)
-    threshold: float
-
 
 app = FastAPI(title='Face Spoofing API', description=app_desc)
 
@@ -21,7 +15,7 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/api/predict")
-async def predict_api(file: UploadFile = File(...), threshold: Optional[float] = None ):
+async def predict_api(file: UploadFile = File(...), threshold: Optional[float] = None):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png", "JPG")
     if not extension:
         return "Image must be jpg or png format!"
